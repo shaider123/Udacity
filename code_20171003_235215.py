1767a8c4e540e32d651db6a206b3f54ce1209401
shaider123 <20198663+shaider123@users.noreply.github.com>
2017-10-03 19:59:41 -0400

    Add files via upload

---
+++ code_20171003_235215.py
@@ -0,0 +1,21 @@
+import webbrowser
+
+
+class Movie():
+
+    '#'provides documentation about the class"Movie"initializing memory space
+'#'"self" is the instance being created
+
+
+def __init__
+(self, movie_title, movie_storyline, poster_image_url, trailer_youtube_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
+        '#'instance method
+
+
+def show_trailer(self):
+    webbrowser.open(self.trailer_youtube_url)
